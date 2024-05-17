a71566608ea0d70fd824007a0841eb953514ead4
Tristan PINTO <tristan.pin81@gmail.com>
2024-05-17 16:52:32 +0200

    Feat: Implement application header

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
 import clsx from "clsx";
+import { Header } from "@/src/features/layout/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,9 +18,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={clsx(inter.className, 'bg-background')}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>{children}</ThemeProvider>
+    <html lang="en" className="h-full">
+      <body className={clsx(inter.className, 'bg-background h-full')}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+          <div className="flex flex-col h-full">
+            <Header></Header>
+            <div className="flex-1 max-w-lg m-auto py-12 w-full">{children}</div>
+          </div>
+        </ThemeProvider>
       </body>
     </html>
   );

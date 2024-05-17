0d0ad2ebd28fe70f183df29c604f0b1e92ab2343
Tristan PINTO <tristan.pin81@gmail.com>
2024-05-17 21:46:41 +0200

    Feat: Add theme toggle button in header

--- src/features/layout/Header.tsx
+++ src/features/layout/Header.tsx
@@ -1,3 +1,5 @@
+import { Button } from '@/components/ui/button'
+import { ThemeToggle } from '@/src/theme/ThemeToggle'
 import React from 'react'
 
 export const Header = async () => {
@@ -5,6 +7,7 @@ export const Header = async () => {
     <header className='border-b border-b-accent'>
         <div className='container flex items-center py-2 max-w-lg m-auto gap-1'>
             <h2 className='text-2xl font-bold mr-auto'>GitThread</h2>
+            <ThemeToggle></ThemeToggle>
         </div>
     </header>
   )

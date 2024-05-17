dfb69a060c10d19332b4d55d2efc21b4efaf658c
Tristan PINTO <tristan.pin81@gmail.com>
2024-05-17 14:46:12 +0200

    Feat: Use different shadcn-ui components

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import Image from "next/image";
 
 export default function Home() {
-  return <div> Hello world !</div>;
+  return <div className="m-4">
+    <Button>Click me</Button>
+    <Input/>
+  </div>;
 }

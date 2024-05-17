b981e70df0b3012200fa7fc6e2faea503e6937d1
Tristan PINTO <tristan.pin81@gmail.com>
2024-05-17 14:32:47 +0200

    Feat: Implement a routable profile page

---
+++ app/profile/page.tsx
@@ -0,0 +1,11 @@
+//  Logic between folder structure and routage
+//  Example:
+//      app/profile/page.tsx --> localhost:3000/profile
+
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>profile</div>
+  )
+}
